9a8dc4d6f24b745b045a7a4f49fb6f68d235829d
Tushar <tusharmathur2411@gmail.com>
2020-08-28 02:57:52 +0530

    Added manage user endpoint.

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -15,6 +15,17 @@ class UserSerializer(serializers.ModelSerializer):
 
             return get_user_model().objects.create_user(**validated_data)
 
+        def update(self, instance, validated_data):
+
+            password = validated_data.pop('password', None)
+            user = super().update(instance, validated_data)
+
+            if password:
+                user.set_password(password)
+                user.save()
+
+            return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
 
